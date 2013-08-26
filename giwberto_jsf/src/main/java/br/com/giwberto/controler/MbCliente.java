bca7fb8206851859e12663a0d48e3c887aef1597
giwberto <giwberto@giwberto-Aspire-5050>
2013-08-25 20:01:47 -0400

    Alterações em Cadastro de Clientes

--- giwberto_jsf/src/main/java/br/com/giwberto/controler/MbCliente.java
+++ giwberto_jsf/src/main/java/br/com/giwberto/controler/MbCliente.java
@@ -23,11 +23,9 @@ import javax.faces.context.FacesContext;
  */
 @ManagedBean
 @SessionScoped
-
 public class MbCliente implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
     private String confereSenha;
     private Cliente cliente = new Cliente();
     private Endereco endereco = new Endereco();
@@ -36,61 +34,80 @@ public class MbCliente implements Serializable {
 
     public MbCliente() {
     }
-    
-    private InterfaceDAO<Cliente> clienteDAO(){
+
+    private InterfaceDAO<Cliente> clienteDAO() {
         InterfaceDAO<Cliente> clienteDAO = new HibernateDAO<Cliente>(Cliente.class, FacesContextUtil.getRequestSession());
         return clienteDAO;
     }
-    
-    private InterfaceDAO<Endereco> enderecoDAO(){
+
+    private InterfaceDAO<Endereco> enderecoDAO() {
         InterfaceDAO<Endereco> enderecoDAO = new HibernateDAO<Endereco>(Endereco.class, FacesContextUtil.getRequestSession());
         return enderecoDAO;
     }
-    
-    public String limpCliente(){
+
+    public String limpCliente() {
         cliente = new Cliente();
-        endereco =  new Endereco();
-       return editCliente();
+        endereco = new Endereco();
+        return editCliente();
     }
-    
-    public String editCliente(){
-        return "/restrict/cadastrarcliente.faces";
+
+    public String editCliente() {
+       return "/restrict/cadastrarcliente.faces";
     }
-    
-    public String addCliente(){
+
+    public String addCliente() {
         Date date = new Date();
-        if(cliente.getIdCliente()== null || cliente.getIdCliente() ==0){
+        if (cliente.getIdCliente() == null || cliente.getIdCliente() == 0) {
             cliente.setDataDeCadastro(date);
             insertCliente();
-        }else{
+        } else {
+           
             updateCliente();
         }
-        
-        limpCliente();
+
         return null;
     }
 
     private void insertCliente() {
-    clienteDAO().save(cliente);
-    endereco.setCliente(cliente);
-    enderecoDAO().save(endereco);
-    FacesContext.getCurrentInstance().addMessage(null,
+        clienteDAO().save(cliente);
+        endereco.setCliente(cliente);
+        enderecoDAO().save(endereco);
+        FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravação efetuada com sucesso", ""));
     }
 
     private void updateCliente() {
-    clienteDAO().update(cliente);
-    endereco.setCliente(cliente);
-    enderecoDAO().update(endereco);
-    FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualização efetuada com sucesso", ""));
-    }
-    
-    public String deleteCliente(){
-        clienteDAO().remove(cliente);
+        System.out.println("A cidade é : " + endereco.getCidade().getIdCidade());
+        System.out.println("O cep é: " + endereco.getCep());
+        System.out.println("O complemento é: " + endereco.getComplemento());
+        System.out.println("O estado é: " + endereco.getEstado().getIdEstado());
+        System.out.println("O cliente é: " + cliente.getNome());
+        System.out.println("O endereço é: " + endereco.getBairro() + " ");
+        System.out.println("O cep é: " + endereco.getCep());
+        System.out.println("O id endereco é: " +endereco.getIdEndereco());
+
+        System.out.println("o sexo é:" + cliente.getSexo());
+        System.out.println("" + cliente.getSenha());
+        System.out.println("O idcliente é: " + cliente.getIdCliente());
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        
+
+        clienteDAO().update(cliente);
+        enderecoDAO().update(endereco);
         FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualização efetuada com sucesso", ""));
-    
+    }
+
+    public String deleteCliente() {
+        clienteDAO().remove(cliente);
+        enderecoDAO().remove(endereco);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualização efetuada com sucesso", ""));
+
         return null;
     }
 
@@ -135,7 +152,4 @@ public class MbCliente implements Serializable {
     public void setConfereSenha(String confereSenha) {
         this.confereSenha = confereSenha;
     }
-
-    
-    
 }
